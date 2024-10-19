8978324ea45bc5329ee3f22ebdd50088f9f5a654
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-10-19 21:33:59 +0500

    Mongodb setup

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const http = require('http')
+
 const server = http.createServer(function(req, res){
     if(req.url === "/getdata"){
-        res.end("No data available here")
+        res.end(`<h1 style="text-align: center; display: flex; align-items: center;">I will do something bigger INSHALLAH</h1>`)
     }
     res.end("Hello World")
 });
